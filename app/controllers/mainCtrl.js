0220acbf0490ddaab72d98e2f3419ad80ed81ec3
deepak <deepak@withstartups.com>
2014-09-26 19:52:13 +0530

    Part of factory working - secondsToMinutes function

--- app/controllers/mainCtrl.js
+++ app/controllers/mainCtrl.js
@@ -2,7 +2,7 @@
   
   //controller starts
   var mainCtrl = function($scope, $interval, timerFactory){
-    var alertSound = new Audio('Buzz.mp3')
+    
     var count = 0;
 
     $scope.breakSeconds = 0;
@@ -12,31 +12,16 @@
     $scope.mainSeconds = 0;
     $scope.mainMinutes = 0;
     $scope.mainCounter = 0;
-
-    $scope.testingFactory = timerFactory.testingFactory();
-
-    $scope.playAudio = function(){
-      alertSound.play();
-    };
-
-    var secondsToMinutes = function(seconds, minutes, counter, duration){
-          $scope[seconds]++;
-          if ($scope[seconds] == 60){
-            $scope[seconds] = 0;
-            $scope[minutes]++;
-            $scope[counter] += 1;
-            if ($scope[counter] == duration){
-              alertSound.play();
-              $scope[counter] = 0;
-            };
-          }
-      };
       
-
+    
     var breakTimer = undefined;
     $scope.startBreakTimer = function(){
-        breakTimer = $interval(function () { secondsToMinutes('breakSeconds', 'breakMinutes', 'breakCounter', 5) }, 100, 300);
+        breakTimer = $interval(function () { 
+          timerFactory.secondsToMinutes($scope, 'breakSeconds', 'breakMinutes', 'breakCounter', 5) }, 100, 300);
       };
+
+
+    //$scope.startBreakTimer = timerFactory.startBreakTimer($scope, 'breakSeconds', 'breakMinutes', 'breakCounter');
 
     $scope.stopBreakTimer = function(){
         $interval.cancel(breakTimer);
@@ -51,7 +36,8 @@
 
     var mainTimer = undefined;
     $scope.startMainTimer = function(){
-      mainTimer = $interval(function () { secondsToMinutes('mainSeconds', 'mainMinutes', 'mainCounter', 25) }, 100, 1500);
+      mainTimer = $interval(function () { 
+        timerFactory.secondsToMinutes($scope, 'mainSeconds', 'mainMinutes', 'mainCounter', 25) }, 100, 1500);
       };
 
     $scope.stopMainTimer = function(){
--- app/services/timerFactory.js
+++ app/services/timerFactory.js
@@ -7,6 +7,27 @@
         return "hello world";
       };
 
+      factory.secondsToMinutes = function(scope, seconds, minutes, counter, duration){
+          var alertSound = new Audio('Buzz.mp3');
+          scope[seconds]++;
+          if (scope[seconds] == 60){
+            scope[seconds] = 0;
+            scope[minutes]++;
+            scope[counter] += 1;
+            if (scope[counter] == duration){
+              alertSound.play();
+              scope[counter] = 0;
+            };
+          }
+      };
+
+      //above this is working
+      
+      var breakTimer = undefined;
+      factory.startBreakTimer = function(scope){
+        breakTimer = $interval(function () { 
+          timerFactory.secondsToMinutes(scope, 'breakSeconds', 'breakMinutes', 'breakCounter', 5) }, 10, 300);
+      };
 
       return factory;
   };
